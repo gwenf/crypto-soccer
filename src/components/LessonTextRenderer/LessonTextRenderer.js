e331c6381bd7736b948ea14bc593c01b4736b009
Anup Vasudevan <rtre84@users.noreply.github.com>
2018-07-29 22:37:57 -0400

    Added to the lessons object. Lessons data gets pulled in from the object.

--- src/components/LessonTextRenderer/LessonTextRenderer.js
+++ src/components/LessonTextRenderer/LessonTextRenderer.js
@@ -23,7 +23,15 @@ let lessons = {
         cardContentTypography: "This impressive paella is a perfect party dish and a fun meal to cook together with\n" +
         "                        your guests. Add 1 cup of frozen peas along with the mussels, if you like.",
         cardContentTypographyTitle: "Method:",
-
+        cardContentTypographyParagraph: "Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10\n" +
+"                            minutes.",
+        cardContentTypographyParagraph2: "Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high\n" +
+          "heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly\n" +
+          "browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving\n" +
+          "chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,\n" +
+          "salt and pepper, and cook, stirring often until thickened and fragrant, about 10\n" +
+          "minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.",
+        cardContentTypographyParagraph3: "Set aside off of the heat to let rest for 10 minutes, and then serve."
     }
 };
 
@@ -42,11 +50,6 @@ export const LessonTextRenderer = (props) => (
         <div style={{float: "right", width: window.visualViewport.width * 0.37, overflow: 'hidden'}}>
             <Card>
                 <CardHeader
-                    avatar={
-                        <Avatar aria-label="Recipe" className={{}}>
-                            R
-                        </Avatar>
-                    }
                     action={
                         <IconButton>
 
@@ -67,22 +70,16 @@ export const LessonTextRenderer = (props) => (
                 <withWidth>
                     <CardContent>
                         <Typography paragraph variant="body2">
-
+                            {lessons[props.lessonName].cardContentTypographyTitle}
                         </Typography>
                         <Typography paragraph>
-                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-                            minutes.
+                            {lessons[props.lessonName].cardContentTypographyParagraph}
                         </Typography>
                         <Typography paragraph>
-                            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-                            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-                            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-                            chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-                            salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-                            minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+                            {lessons[props.lessonName].cardContentTypographyParagraph2}
                         </Typography>
                         <Typography>
-                            Set aside off of the heat to let rest for 10 minutes, and then serve.
+                            {lessons[props.lessonName].cardContentTypographyParagraph3}
                         </Typography>
                     </CardContent>
                 </withWidth>
